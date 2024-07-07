0de0fb399144bef4a400bc9f34ce1adf6c21b7f7
mickasmt <sermont.mickael@hotmail.fr>
2024-07-07 15:45:53 +0200

    docs: update preview landing

--- components/sections/preview-landing.tsx
+++ components/sections/preview-landing.tsx
@@ -6,11 +6,19 @@ export default function PreviewLanding() {
   return (
     <div className="pb-6 sm:pb-20">
       <MaxWidthWrapper>
-        <div className="rounded-xl md:bg-muted/30 md:p-3.5 md:ring-1 md:ring-inset md:ring-border">
-          <div className="relative aspect-video overflow-hidden rounded-xl border md:rounded-lg">
+        <div className="h-auto rounded-xl md:bg-muted/30 md:p-3.5 md:ring-1 md:ring-inset md:ring-border">
+          <div className="relative overflow-hidden rounded-xl border md:rounded-lg">
             <Image
-              className="size-full object-cover object-center dark:opacity-85 dark:invert"
-              src="/_static/blog/blog-post-3.jpg"
+              className="flex size-full object-contain object-center dark:hidden"
+              src="/_static/images/light-preview.jpg"
+              alt="preview landing"
+              width={2000}
+              height={1000}
+              priority={true}
+            />
+            <Image
+              className="hidden size-full object-contain object-center dark:flex"
+              src="/_static/images/dark-preview.jpg"
               alt="preview landing"
               width={2000}
               height={1000}
